fc06479a1f47576efd54ef65a1d0221042210597
Javidt <71132058+Javidt@users.noreply.github.com>
2021-11-25 20:21:40 +0100

    Create script.sh

---
+++ scripts/script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo 'Script para automatizar la lectura de los ficheros de datos de sensores'
+
+if [ $# -eq 1 ];
+then
+    LINEAS=1000
+elif [ $# -eq 2 ];
+then
+    LINEAS=$2
+    echo $LINEAS
+else
+    echo 'Número de parámetros incorrecto' 1>&2
+    echo 'Introduzca el nombre del fichero y el intervalo de líneas' 1>&2
+    exit 1
+fi
+#Comprobamos que el fichero pasado como parámetro existe
+test -f $1
+if [ $? -ne 0 ];
+then
+    echo 'El fichero pasado como parámetro no existe' 1>&2
+    exit 2
+fi
+
+cat $1 > aux
+
+test -f fich
+if [ $? -eq 0 ]
+then
+    rm fich
+fi
+touch fich
+
+CONT=0
+FIN=0
+while IPFS= read -u10 line
+do
+    if [ $CONT -eq $LINEAS ];
+    then
+        CONT=0
+        read -n 1 -s -r -p "Pulsar tecla"
+        #sleep 1
+    fi
+    CONT=$((CONT + 1))
+    FIN=$((FIN + 1))
+    echo $line >> fich
+done 10< aux
+rm aux
+
+#read -n 1 -s -r -p "Pulsar tecla"
+#echo
+#echo "Num lineas: " $FIN
