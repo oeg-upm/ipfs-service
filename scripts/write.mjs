fefcedd7a608e2be2f019a30b08f4073e2bc4f44
Javier <71132058+Javidt@users.noreply.github.com>
2021-12-14 00:49:08 +0100

    Update write.mjs

--- scripts/write.mjs
+++ scripts/write.mjs
@@ -1,31 +1,66 @@
 import fetch from 'node-fetch'
-import fs from 'fs'
+import fs from 'fs';
 
-const optionsGet = {
-    method: "GET",
+
+let optionsPost = {
+    method: "POST",
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    body: JSON.stringify({
+            content: 'Subimos este contenido'
+    })
+}
+var cid;
+//Subimos y publicamos el fichero
+try {
+    let data = fs.readFileSync('datos.csv');
+    let contenido = data.toString('UTF8');
+    fetch("http://localhost:4040/file", optionsPost = {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: contenido
+        })
+    })
+    .then(response => response.text())
+    .then(data => {
+        const json = JSON.parse(data)
+        cid = json.cid
+        console.log(cid)
+        fetch("http://localhost:4040/file/ipns", optionsPost = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cid: cid
+            })
+        })
+        .then(response => response.text())
+        .then(data => {
+            const json = JSON.parse(data)
+            //console.log(json)
+            });
+        });
+} catch (err) {
+    console.error(err);
 };
-// Petición HTTP de un get
-const key = "k51qzi5uqu5dk9bwkaayajs0vnw0gielvkxq48og9y2igjdv1y0apbr3fy1fmi";
-const hash = "QmWWM1ru8CZ8Qih2m8JNyWnZP8hBH9C6DVw5WhYg2P6rvQ";
 
-
-fetch("http://localhost:4040/file/ipns/" + key, optionsGet)
+//Avisamos de que el fichero se ha actualizado
+fetch("http://localhost:4040/pubsub/ipns", optionsPost = {
+        method: "POST",
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: "El fichero ha sido actualizado."
+        })
+    })
     .then(response => response.text())
     .then(data => {
     //Procesar los datos
-    const json = JSON.parse(data);
-    const aux = json.content
-    const content = aux.toString('UTF8');
-    //console.log(json)
-    //Escribimos el contenido recibido en un fichero destino
-    fs.writeFileSync('destino.csv', content, {
-        encoding: "utf8",
-        flag: "a+",
-        mode: 0o666
-    });
+    console.log("Nos suscribimos al topic: ipns")
 });
-
-
