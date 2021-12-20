887b6069b0ec00c9a3bc56daf767bf79b388ad99
Javier <71132058+Javidt@users.noreply.github.com>
2021-12-20 14:32:36 +0100

    Update write.mjs

--- scripts/write.mjs
+++ scripts/write.mjs
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch'
 import fs from 'fs';
+import { exec }  from "child_process"
 
+const num = process.argv[2];
 
 let optionsPost = {
     method: "POST",
@@ -14,8 +16,11 @@ let optionsPost = {
 var cid;
 //Subimos y publicamos el fichero
 try {
-    let data = fs.readFileSync('datos.csv');
+    //Fichero que vamos a publicar
+    let data = fs.readFileSync('final' + num + '.csv')
+    console.info(data)
     let contenido = data.toString('UTF8');
+    //console.log(contenido);
     fetch("http://localhost:4040/file", optionsPost = {
         method: "POST",
         headers: {
@@ -27,9 +32,10 @@ try {
     })
     .then(response => response.text())
     .then(data => {
+        console.log("Hemos leído los datos de sensores");
         const json = JSON.parse(data)
         cid = json.cid
-        console.log(cid)
+        console.log("cid: " + cid)
         fetch("http://localhost:4040/file/ipns", optionsPost = {
             method: "POST",
             headers: {
@@ -42,25 +48,25 @@ try {
         .then(response => response.text())
         .then(data => {
             const json = JSON.parse(data)
-            //console.log(json)
+            console.log("Key: " + json.key)
+            //Avisamos de que el fichero se ha actualizado
+            fetch("http://localhost:4040/pubsub/ipns" + num, optionsPost = {
+                method: "POST",
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    content: cid
+                })
+            })
+            .then(response => response.text())
+            .then(data => {
+            //Procesar los datos
+            console.log("Fichero actualizado")
             });
         });
+    });
 } catch (err) {
     console.error(err);
 };
 
-//Avisamos de que el fichero se ha actualizado
-fetch("http://localhost:4040/pubsub/ipns", optionsPost = {
-        method: "POST",
-        headers : {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            content: "El fichero ha sido actualizado."
-        })
-    })
-    .then(response => response.text())
-    .then(data => {
-    //Procesar los datos
-    console.log("Nos suscribimos al topic: ipns")
-});
