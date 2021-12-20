0ccbd810d6f1df2681f16c2201682511d6f74931
Javier <71132058+Javidt@users.noreply.github.com>
2021-12-20 16:31:51 +0100

    Update write.mjs

--- maq1/write.mjs
+++ maq1/write.mjs
@@ -18,7 +18,6 @@ var cid;
 try {
     //Fichero que vamos a publicar
     let data = fs.readFileSync('final' + num + '.csv')
-    console.info(data)
     let contenido = data.toString('UTF8');
     //console.log(contenido);
     fetch("http://localhost:4040/file", optionsPost = {
@@ -31,24 +30,10 @@ try {
         })
     })
     .then(response => response.text())
-    .then(data => {
-        console.log("Hemos leído los datos de sensores");
-        const json = JSON.parse(data)
-        cid = json.cid
-        console.log("cid: " + cid)
-        fetch("http://localhost:4040/file/ipns", optionsPost = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                cid: cid
-            })
-        })
-        .then(response => response.text())
         .then(data => {
             const json = JSON.parse(data)
-            console.log("Key: " + json.key)
+            const cid = json.cid
+            console.log("CID: " + json.cid)
             //Avisamos de que el fichero se ha actualizado
             fetch("http://localhost:4040/pubsub/ipns" + num, optionsPost = {
                 method: "POST",
@@ -65,7 +50,6 @@ try {
             console.log("Fichero actualizado")
             });
         });
-    });
 } catch (err) {
     console.error(err);
 };
