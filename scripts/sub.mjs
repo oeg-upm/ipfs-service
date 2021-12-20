acd86e1ea612ac1f94504dd28c05d779ee81471b
Javier <71132058+Javidt@users.noreply.github.com>
2021-12-20 14:32:21 +0100

    Update sub.mjs

--- scripts/sub.mjs
+++ scripts/sub.mjs
@@ -2,19 +2,41 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 //Script para suscribirse al topic ipns
-const optionsPost = {
+var optionsPost = {
     method: "POST",
     headers: {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-    	topic: "ipns"
+    	topic: "ipns01"
     })
 };
 
-fetch("http://localhost:4040/pubsub", optionsPost)
+fetch("http://localhost:4040/pubsub", optionsPost ={
+method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    	topic: "ipns01"
+    })
+})
     .then(response => response.text())
     .then(data => {
     //Procesar los datos
-    console.log("Nos suscribimos al topic: ipns")
+    console.log("Nos suscribimos al topic: ipns01")
+});
+fetch("http://localhost:4040/pubsub", optionsPost ={
+method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    	topic: "ipns02"
+    })
+})
+    .then(response => response.text())
+    .then(data => {
+    //Procesar los datos
+    console.log("Nos suscribimos al topic: ipns02")
 });
