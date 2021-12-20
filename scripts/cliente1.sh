2c577caec0b2ba7e87fd851591b613047377fb44
Javier <71132058+Javidt@users.noreply.github.com>
2021-12-20 16:36:41 +0100

    Delete scripts directory

--- scripts/cliente1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Script que ejecuta la lectura del nodo1.
-
-
-start=$SECONDS
-node lecturaLineas.mjs scripts/int_analog01.csv &
-node lecturaLineas.mjs scripts/int_analog02.csv 
-duration=$(( SECONDS - start ))
-echo "duracion: " $duration
