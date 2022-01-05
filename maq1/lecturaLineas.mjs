b3c89f06c2d44dddaad0b798f4a9644c97652983
Javier <71132058+Javidt@users.noreply.github.com>
2022-01-05 12:57:55 +0100

    Update lecturaLineas.mjs

--- maq1/lecturaLineas.mjs
+++ maq1/lecturaLineas.mjs
@@ -2,32 +2,37 @@ import fs, { read } from 'fs'
 import { exec, fork } from 'child_process'
 import readline from 'readline'
 
-
-
+var num = "00";
 //const archivo='scripts/int_analog01.csv'
 const archivo = process.argv[2];
-const num = archivo.replace("scripts/int_analog",'').replace('.csv','')
+if(archivo == "pro_power.csv") num = "03";
+else num = archivo.replace("int_analog",'').replace('.csv','')
 
 const readable = fs.createReadStream(archivo)
-const escritor = fs.createWriteStream('final' + num + '.csv')
+//const escritor = fs.createWriteStream('final' + num + '.csv')
 let bloques = 0;
 var rl = readline.createInterface({
     input: readable
 });
 
 var lineas = 0;
+var bloq = 0;
+var content = "";
+var cont = 0;
 rl.on('line', (line) => {
     //console.log(line)
     lineas += 1;
-    var content = line + '\n'
-    fs.writeFileSync('final' + num + '.csv', content, {
-        encoding: "utf8",
-        flag: "a+",
-        mode: 0o666
-    });
+
+    bloq += 1;
+    content += (line + '\n')
     if(lineas == 400000) {
+        cont += 400000
         rl.pause()
-        
+        fs.writeFileSync('final' + num + '.csv', content, {
+            encoding: "utf8",
+            flag: "w+",
+            mode: 0o666
+        });
         console.info("Pausamos la ejecución")
         let comando = "node write.mjs " + num
         exec(comando, (error, stdout, stderr) => {
@@ -41,12 +46,19 @@ rl.on('line', (line) => {
             }
                 console.log(`stdout: ${stdout}`)
                 lineas = 0;
-                rl.resume();
+                rl.resume();                
         });
     }
 });
 rl.on('close', () => {
-    console.info("Último bloque del fichero")
+    fs.writeFileSync('final' + num + '.csv', content, {
+        encoding: "utf8",
+        flag: "w+",
+        mode: 0o666
+    });
+    console.info("Último bloque del fichero: " + num)
+    console.info("Número de líneas copiadas: " + bloq)
+    content = ""
     let comando = "node write.mjs " + num
     exec(comando, (error, stdout, stderr) => {
         if (error) {
